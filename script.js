fedcf52df80c1e6697e1d5852cb2fb5d2a449151
Ken Yeh <yehken@gmail.com>
2024-09-30 22:09:03 -0700

    store document variables

--- script.js
+++ script.js
@@ -7,5 +7,19 @@
  * Add some validation for form
  */
 
+// body style
 // load Source Code Pro google font
 document.body.style.fontFamily = 'Source Code Pro', 'monospace';
+document.body.style.backgroundColor = 'azure';
+// set font for everything else
+const formElements = document.querySelectorAll('button, input, select');
+formElements.forEach((element) => {
+    element.style.fontFamily = "'Source Code Pro', 'monospace'";
+});
+
+// cache elements
+const taskForm = document.getElementById('taskForm');
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+const completedTaskList = document.getElementById('completedTaskList');
+const clearTaskBtn = document.querySelector('#clearTaskBtn');
