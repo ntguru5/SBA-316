41ec2698e20346e306c4e98746d3f6c1136fcbd0
Ken Yeh <yehken@gmail.com>
2024-09-30 20:18:01 -0700

    start pseudocode

---
+++ script.js
@@ -0,0 +1,8 @@
+/**
+ * PSEUDOCODE
+ * Create task list form with different priorities
+ * Checkbox should strikethrough task
+ * Be able to delete task
+ * Be able to clear all tasks
+ * Add some validation for form
+ */
